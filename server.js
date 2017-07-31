14caa9f47ad97d191b4dcd6d8b37754d03d94a70
anand chaudhary <nndchaudhary8@gmail.com>
2017-07-31 09:32:43 +0530

    entry point check heroku

--- server.js
+++ server.js
@@ -1,35 +1,42 @@
 var express = require('express');
-var mysql = require('mysql');
+// var mysql = require('mysql');
 var app = express();
-var port = 3001;
+var port = 8000;
 
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "node",
 
-});
+app.get("/",function(req,res){
+  res.send("hello world i am here");
+})
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-  var sql = "INSERT INTO users (user,pwd) VALUES ? ";
-  var values = [
-  		["anakshiant","player"],
-  		["rama","krishna"],
-  		['chahat',"kora"],
-  ]
-  con.query(sql,[values],function(err,result){
-  	if(err){
-  		console.log(err);
-  	}
-  	console.log("table created"+result.insertId);
-  });
+app.listen(port);
 
-  con.query("SELECT * FROM users",function(err,res){
-  		console.log(res);
-  });
-});
+// var con = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "node",
+
+// });
+
+// con.connect(function(err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+//   var sql = "INSERT INTO users (user,pwd) VALUES ? ";
+//   var values = [
+//   		["anakshiant","player"],
+//   		["rama","krishna"],
+//   		['chahat',"kora"],
+//   ]
+//   con.query(sql,[values],function(err,result){
+//   	if(err){
+//   		console.log(err);
+//   	}
+//   	console.log("table created"+result.insertId);
+//   });
+
+//   con.query("SELECT * FROM users",function(err,res){
+//   		console.log(res);
+//   });
+// });
 
 
